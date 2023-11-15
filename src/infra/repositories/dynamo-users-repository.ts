df9c78aa773cd24ed58c7f1f25c13ceef3d5fe7b
Guilherme Bolfe <bolfeguilherme@gmail.com>
2023-11-15 18:31:23 -0300

    feat: add get user

--- src/infra/repositories/dynamo-users-repository.ts
+++ src/infra/repositories/dynamo-users-repository.ts
@@ -1,5 +1,9 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb'
-import { User } from '@domain/entities'
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+} from '@aws-sdk/client-dynamodb'
+import { UniqueEntityID, User } from '@domain/entities'
 import { UsersRepository } from '@domain/repositories'
 import { env } from '../env'
 
@@ -12,7 +16,7 @@ export class DynamoUsersRepository implements UsersRepository {
   }
 
   async create(user: User): Promise<void> {
-    const commandPutItem = new PutItemCommand({
+    const putItemCommand = new PutItemCommand({
       TableName: this.tableName,
       Item: {
         id: { S: user.id.toValue() },
@@ -24,10 +28,32 @@ export class DynamoUsersRepository implements UsersRepository {
       },
     })
 
-    await this.client.send(commandPutItem)
+    await this.client.send(putItemCommand)
   }
 
   async get(id: string): Promise<User | undefined> {
-    throw new Error('Method not implemented.')
+    const getItemCommand = new GetItemCommand({
+      TableName: this.tableName,
+      Key: {
+        id: { S: id },
+      },
+    })
+
+    const responseGet = await this.client.send(getItemCommand)
+
+    if (!responseGet.Item) {
+      return undefined
+    }
+
+    return User.create(
+      {
+        email: responseGet.Item.email.S ?? '',
+        name: responseGet.Item.name.S ?? '',
+        passwordHash: responseGet.Item.passwordHash.S ?? '',
+        createdAt: new Date(responseGet.Item.createdAt.S ?? ''),
+        updatedAt: new Date(responseGet.Item.updatedAt.S ?? ''),
+      },
+      new UniqueEntityID(responseGet.Item.id.S),
+    )
   }
 }
